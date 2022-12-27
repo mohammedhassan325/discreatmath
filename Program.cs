744d4feb93a4f619e42aa11eedf69a786300caef
mohammedhassan325 <121507636+mohammedhassan325@users.noreply.github.com>
2022-12-27 18:59:43 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,29 +1,38 @@
-﻿namespace ConsoleApp1
+﻿namespace ConsoleApp2
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
-            int First, second, X, Y, sum;
-            Console.WriteLine("Enter the first");
-            First = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Enter the second");
-            second = int.Parse(Console.ReadLine());
-            for (X = First; X <= second; X++)
+        {//Project 1
+         // write a C# program to writ the prime numbers from ni to n2.
+         //The Input: n1 and n2
+         //The Output: The list of the prime numbers from ni to n2
+         //Example: For the input numbers: n1 = 5 and n2 = 18, the output should be
+         // 5 1/7 //11 //13 //17
+            Console.WriteLine("enter n1");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter n2");
+            int n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("the prime numbers between {0} and {1}", n1, n2);
+            for (int i = n1; i <= n2; i++)
             {
-                Y = 1;
-                sum = 0;
-                while (Y < X)
-                {
-                    if (X % Y == 0)
-                        sum = sum + Y;
-                    Y++;
-                }
-                if (sum == X && X != 0)
+                int counter = 0;
+                for (int j = 2; j <= i / 2; j++)
+                    if (i % j == 0)
+                    { }
+                counter++;
+                break;
 
-                    Console.WriteLine("{0}", X);
+                if (counter == 0 && i != 1)
+                {
+                    Console.WriteLine("{0}", i);
+                }
+
+
             }
+
         }
+
     }
+
 }
